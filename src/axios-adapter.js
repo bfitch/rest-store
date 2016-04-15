12efc6afb4c1b0844772ef7598f1f96f985694cc
Brian Fitch <brianmfitch@gmail.com>
2016-04-15 10:46:26 -0400

    DRY up axios adapter instance methods

--- src/axios-adapter.js
+++ src/axios-adapter.js
@@ -2,56 +2,52 @@ import ajax from 'axios';
 import {isEmpty} from './utils';
 
 export default function(axios = ajax) {
-  return (options) => {
-    return {
-      find(url, params, headers) {
-        const axiosOptions = {method: 'get', url, params, headers};
-        return request(axiosOptions, options);
-      },
+  const mapping = {find: 'get', create: 'post', update: 'put', delete: 'delete'};
 
-      create(url, data, params, headers) {
-        const axiosOptions = {method: 'post', url, data, params, headers};
-        return request(axiosOptions, options);
-      },
+  const instance = Object.keys(mapping).reduce((instance, apiMethod) => {
+    const httpAction = mapping[apiMethod];
 
-      update(url, data, params, headers) {
-        const axiosOptions = {method: 'put', url, data, params, headers};
-        return request(axiosOptions, options);
-      },
-
-      delete(url, params, headers) {
-        const axiosOptions = {method: 'delete', url, params, headers};
-        return request(axiosOptions, options);
-      }
+    instance[apiMethod] = function(url, params, headers) {
+      const axiosOptions = {method: httpAction, url, params, headers};
+      return request(axiosOptions, this.options);
     }
+    return instance;
+  },{});
 
-    function request(axiosOptions, {root, model}) {
-      return axios(axiosOptions).then(response => {
-          return normalizeResponse(response.data, {root});
-        })
-        .then(data => {
-          return !model ? data : buildModels(data, model);
-        })
+  return Object.assign(instance, {
+    http: axios,
+    options: {},
+    setConfig(options) {
+      this.options = options;
     }
+  });
 
-    function normalizeResponse(response, {root}) {
-      if (root) {
-        const msg = 'Expecting a root key in ajax response. But the response was empty.'
-        if (isEmpty(response)) throw new Error(msg);
+  function request(axiosOptions, {root, model}) {
+    return axios(axiosOptions).then(response => {
+        return normalizeResponse(response.data, {root});
+      })
+      .then(data => {
+        return !model ? data : buildModels(data, model);
+      })
+  }
 
-        const rootKey = Object.keys(response).pop();
-        return response[rootKey];
-      } else {
-        return response;
-      }
+  function normalizeResponse(response, {root}) {
+    if (root) {
+      const msg = 'Expecting a root key in ajax response. But the response was empty.'
+      if (isEmpty(response)) throw new Error(msg);
+
+      const rootKey = Object.keys(response).pop();
+      return response[rootKey];
+    } else {
+      return response;
     }
+  }
 
-    function buildModels(data, model) {
-      if (Array.isArray(data)) {
-        return data.map(attrs => new model(attrs));
-      } else {
-        return new model(data);
-      }
+  function buildModels(data, model) {
+    if (Array.isArray(data)) {
+      return data.map(attrs => new model(attrs));
+    } else {
+      return new model(data);
     }
   }
 }
