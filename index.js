95b354085c0842f27b5aaf443579270b47147d8e
Brian Fitch <brianmfitch@gmail.com>
2016-07-01 13:15:21 +0200

    Add standard linter.

--- index.js
+++ index.js
@@ -1,181 +1,149 @@
-import uuid from 'node-uuid';
-import axiosAdapter from './src/axios-adapter';
-import jsStoreAdapter from './src/js-store-adapter';
-import cerebralStoreAdapter from './src/cerebral-store-adapter';
-import config from './src/configuration';
-import {isEmpty} from './src/utils';
-const {assign} = Object;
+import uuid from 'node-uuid'
+import axiosAdapter from './src/axios-adapter'
+import jsStoreAdapter from './src/js-store-adapter'
+import cerebralStoreAdapter from './src/cerebral-store-adapter'
+import config from './src/configuration'
+import { isEmpty } from './src/utils'
+const {assign} = Object
 
-export function restStore(mappings, storeAdapter, ajaxAdapter = axiosAdapter()) {
-  if (!storeAdapter) throw new Error('No storeAdapter. You must provide an in-memory store');
+export function restStore (mappings, storeAdapter, ajaxAdapter = axiosAdapter()) {
+  if (!storeAdapter) throw new Error('No storeAdapter. You must provide an in-memory store')
 
   return {
     cache: storeAdapter.cache,
     http: ajaxAdapter.http,
 
-    find(path, clientQuery, httpOptions) {
-      const options = config(mappings, path, 'find', clientQuery, httpOptions);
-      const {
-        url,
-        params,
-        headers,
-        force,
-        query,
-        identifier,
-        transformResponse
-      } = options;
+    find (path, clientQuery, httpOptions) {
+      const options = config(mappings, path, 'find', clientQuery, httpOptions)
+      const {url, params, headers, force, query, identifier, transformResponse} = options
 
       if (force) {
         return ajaxAdapter.find(url, params, headers)
           .then(fetchedData => {
-            const transformedData = transformResponse(fetchedData, storeAdapter, options);
-            return storeAdapter.update(path, transformedData[identifier], transformedData, {replace: true});
+            const transformedData = transformResponse(fetchedData, storeAdapter, options)
+            return storeAdapter.update(path, transformedData[identifier], transformedData, {replace: true})
           })
       } else {
         return storeAdapter.get(path, query).then(data => {
-          if (data) return data;
+          if (data) return data
 
           return ajaxAdapter.find(url, params, headers)
             .then(data => storeAdapter.insert(path, transformResponse(data, storeAdapter, options)))
-        });
+        })
       }
     },
 
-    findAll(path, clientQuery, httpOptions) {
-      const options = config(mappings, path, 'findAll', clientQuery, httpOptions);
-      const {
-        url,
-        params,
-        headers,
-        force,
-        query,
-        identifier,
-        transformResponse
-      } = options;
+    findAll (path, clientQuery, httpOptions) {
+      const options = config(mappings, path, 'findAll', clientQuery, httpOptions)
+      const {url, params, headers, force, query, transformResponse} = options
 
       if (force) {
         return ajaxAdapter.find(url, params, headers).then(data => {
-          const transformedData = transformResponse(data, storeAdapter, options);
-          return storeAdapter.updateAll(path, transformedData, {replace: true});
-        });
+          const transformedData = transformResponse(data, storeAdapter, options)
+          return storeAdapter.updateAll(path, transformedData, {replace: true})
+        })
       } else {
         return storeAdapter.getCollection(path, query).then(data => {
-          if (data) return data;
+          if (data) return data
 
           return ajaxAdapter.find(url, params, headers)
             .then(data => {
-              const transformedData = transformResponse(data, storeAdapter, options);
-              return storeAdapter.updateAll(path, transformResponse(data), {replace: true});
-            });
-        });
+              const transformedData = transformResponse(data, storeAdapter, options)
+              return storeAdapter.updateAll(path, transformedData, {replace: true})
+            })
+        })
       }
     },
 
-    create(path, attributes, options = {}) {
-      const opts = config(mappings, path, 'create', {}, assign({}, attributes, options));
-      const {
-        url,
-        params,
-        headers,
-        identifier,
-        transformResponse
-      } = opts;
+    create (path, attributes, options = {}) {
+      const opts = config(mappings, path, 'create', {}, assign({}, attributes, options))
+      const {url, params, headers, transformResponse} = opts
 
       if (options.wait) {
         return ajaxAdapter.create(url, attributes, params, headers)
-          .then(data => storeAdapter.insert(path, transformResponse(data, storeAdapter, options)));
+          .then(data => storeAdapter.insert(path, transformResponse(data, storeAdapter, options)))
       } else {
-        const _cid = uuid.v4();
-        const optimisticUpdate = assign({}, attributes, {_cid});
+        const _cid = uuid.v4()
+        const optimisticUpdate = assign({}, attributes, {_cid})
 
         return storeAdapter.insert(path, optimisticUpdate).then(update => {
           return ajaxAdapter.create(url, attributes, params, headers)
             .then(data => {
-              const transformedData = transformResponse(data, storeAdapter, options);
-              return storeAdapter.updateWhere(path, {_cid}, transformedData, {replace: true});
+              const transformedData = transformResponse(data, storeAdapter, options)
+              return storeAdapter.updateWhere(path, {_cid}, transformedData, {replace: true})
             })
             .catch(response => {
-              storeAdapter.updateWhere(path, {_cid}, null);
-              throw response;
-            });
-          });
+              storeAdapter.updateWhere(path, {_cid}, null)
+              throw response
+            })
+        })
       }
     },
 
-    update(path, clientQuery, attributes, options = {}) {
-      const opts = config(mappings, path, 'update', clientQuery, assign({}, attributes, options));
-      const {
-        url,
-        params,
-        headers,
-        identifier,
-        transformResponse
-      } = opts;
+    update (path, clientQuery, attributes, options = {}) {
+      const opts = config(mappings, path, 'update', clientQuery, assign({}, attributes, options))
+      const {url, params, headers, identifier, transformResponse} = opts
 
       if (options.wait) {
         return ajaxAdapter.update(url, attributes, params, headers).then(data => {
-          const transformedData = transformResponse(data, storeAdapter, options);
-          return storeAdapter.update(path, transformedData[identifier], transformedData, {replace: true});
-        });
+          const transformedData = transformResponse(data, storeAdapter, options)
+          return storeAdapter.update(path, transformedData[identifier], transformedData, {replace: true})
+        })
       } else {
-        const _cid             = uuid.v4();
-        const optimisticUpdate = assign({}, attributes, {_cid});
-        let original           = null;
+        const _cid = uuid.v4()
+        const optimisticUpdate = assign({}, attributes, {_cid})
+        let original = null
 
-        storeAdapter.get(path, clientQuery).then(data => original = data);
+        storeAdapter.get(path, clientQuery).then(data => {
+          original = data
+        })
 
         return storeAdapter.update(path, attributes[identifier], optimisticUpdate, {replace: true})
           .then(() => {
             return ajaxAdapter.update(url, attributes, params, headers)
               .then(data => {
-                const transformedData = transformResponse(data, storeAdapter, options);
-                return storeAdapter.updateWhere(path, {_cid}, transformedData, {replace: true});
+                const transformedData = transformResponse(data, storeAdapter, options)
+                return storeAdapter.updateWhere(path, {_cid}, transformedData, {replace: true})
               })
               .catch(response => {
-                storeAdapter.updateWhere(path, {_cid}, original, {replace: true});
-                throw response;
-              });
-          });
+                storeAdapter.updateWhere(path, {_cid}, original, {replace: true})
+                throw response
+              })
+          })
       }
     },
 
-    delete(path, clientQuery, options = {}) {
-      const opts = config(mappings, path, 'delete', clientQuery, options);
-      const {
-        url,
-        params,
-        headers,
-        identifier,
-        transformResponse
-      } = opts;
+    delete (path, clientQuery, options = {}) {
+      const opts = config(mappings, path, 'delete', clientQuery, options)
+      const {url, params, headers, identifier, transformResponse} = opts
 
       return storeAdapter.get(path, clientQuery).then(data => {
         if (isEmpty(data)) {
           const msg = `No data to delete at path: ${path} with query: ${JSON.stringify(clientQuery)}`
-          throw new Error(msg);
+          throw new Error(msg)
         } else {
-          return data;
+          return data
         }
       })
-      .then(data => {
-        if (options.wait) {
-          return ajaxAdapter.delete(url, data, params, headers).then(deleted => {
-            const transformedData = transformResponse(deleted, storeAdapter, options);
-            return storeAdapter.update(path, transformedData[identifier]);
-          });
-        } else {
-          return storeAdapter.update(path, data[identifier]).then(() => {
-            return ajaxAdapter.delete(url, data, params, headers)
-              .then(response => transformResponse(response, storeAdapter, options))
-              .catch(response => {
-                storeAdapter.insert(path, assign({}, data));
-                throw response;
-              });
-          })
-        }
-      });
+        .then(data => {
+          if (options.wait) {
+            return ajaxAdapter.delete(url, data, params, headers).then(deleted => {
+              const transformedData = transformResponse(deleted, storeAdapter, options)
+              return storeAdapter.update(path, transformedData[identifier])
+            })
+          } else {
+            return storeAdapter.update(path, data[identifier]).then(() => {
+              return ajaxAdapter.delete(url, data, params, headers)
+                .then(response => transformResponse(response, storeAdapter, options))
+                .catch(response => {
+                  storeAdapter.insert(path, assign({}, data))
+                  throw response
+                })
+            })
+          }
+        })
     }
   }
 }
 
-export {jsStoreAdapter, cerebralStoreAdapter};
+export { jsStoreAdapter, cerebralStoreAdapter }
--- src/cerebral-store-adapter.js
+++ src/cerebral-store-adapter.js
@@ -1,159 +1,162 @@
-import Model from 'cerebral-model-baobab';
-import config from './configuration';
-import {isEmpty} from './utils';
-const {isArray} = Array;
-const {assign}  = Object;
-const NOT_FOUND = -1;
+import Model from 'cerebral-model-baobab'
+import config from './configuration'
+import { isEmpty } from './utils'
+const {isArray} = Array
+const {assign} = Object
+const NOT_FOUND = -1
 
-export default function(model = Model(), mappings = {}) {
-  const store = model({on(){}});
+export default function (model = Model(), mappings = {}) {
+  const store = model({on () {}})
 
   return {
     cache: store,
 
-    get(pathString, query) {
-      const path = pathString.split('.');
-      if (isEmpty(query)) throw new Error('You must provide a query when getting items from the store');
-      return Promise.resolve(queryStore('find', store.accessors.get(path), query));
+    get (pathString, query) {
+      const path = pathString.split('.')
+      if (isEmpty(query)) throw new Error('You must provide a query when getting items from the store')
+      return Promise.resolve(queryStore('find', store.accessors.get(path), query))
     },
 
-    getCollection(pathString, query) {
-      const path = pathString.split('.');
-      if (!isArray(store.accessors.get(path))) throw new Error(
-        `getCollection() requies the path: '${pathString}' to be an array. Is of type: ${typeof store.accessors.get(path)}`);
+    getCollection (pathString, query) {
+      const path = pathString.split('.')
+      if (!isArray(store.accessors.get(path))) {
+        throw new Error(`getCollection() requies the path: '${pathString}' to be an array. ` +
+          `Is of type: ${typeof store.accessors.get(path)}`)
+      }
 
-      return Promise.resolve(queryStore('filter', store.accessors.get(path), query));
+      return Promise.resolve(queryStore('filter', store.accessors.get(path), query))
     },
 
-    insert(pathString, attrs) {
-      const path = pathString.split('.');
+    insert (pathString, attrs) {
+      const path = pathString.split('.')
 
-      if (!isArray(store.accessors.get(path)) && isArray(attrs)) throw new Error(
-        `Store path: ${path} must be an array if adding a collection.`);
+      if (!isArray(store.accessors.get(path)) && isArray(attrs)) {
+        throw new Error(`Store path: ${path} must be an array if adding a collection.`)
+      }
 
       if (isArray(attrs)) {
-        store.mutators.concat(path, attrs);
+        store.mutators.concat(path, attrs)
       } else if (!isArray(store.accessors.get(path))) {
-        store.mutators.set(path, attrs);
+        store.mutators.set(path, attrs)
       } else {
-        store.mutators.push(path, attrs);
+        store.mutators.push(path, attrs)
       }
-      return Promise.resolve(attrs);
+      return Promise.resolve(attrs)
     },
 
-    update(pathString, id, attrs = null, options = {replace: false}) {
-      const path = pathString.split('.');
+    update (pathString, id, attrs = null, options = {replace: false}) {
+      const path = pathString.split('.')
 
-      if (isArray(attrs)) throw new Error('ArgumentError: update() cannot set an array. Use updateAll() instead.');
-      const {identifier} = config(mappings, pathString);
-      let updated;
+      if (isArray(attrs)) throw new Error('ArgumentError: update() cannot set an array. Use updateAll() instead.')
+      const {identifier} = config(mappings, pathString)
+      let updated
 
       if (isArray(store.accessors.get(path))) {
-        updated = updateCollection(store, pathString, id, identifier, attrs, options.replace);
+        updated = updateCollection(store, pathString, id, identifier, attrs, options.replace)
       } else {
-        updated = updateObject(store, pathString, attrs, options.replace);
+        updated = updateObject(store, pathString, attrs, options.replace)
       }
-      return Promise.resolve(updated);
+      return Promise.resolve(updated)
     },
 
-    updateAll(pathString, collection, options = {}) {
-      const path = pathString.split('.');
+    updateAll (pathString, collection, options = {}) {
+      const path = pathString.split('.')
 
-      if (!isArray(store.accessors.get(path))) throw new Error(`Store path: ${pathString} must be an array when adding a collection.`);
-      if (isEmpty(collection)) throw new Error(`updateAll() should not be used to update the store path: ${pathString} with an empty collection.`);
+      if (!isArray(store.accessors.get(path))) throw new Error(`Store path: ${pathString} must be an array when adding a collection.`)
+      if (isEmpty(collection)) throw new Error(`updateAll() should not be used to update the store path: ${pathString} with an empty collection.`)
 
-      const {identifier} = config(mappings, pathString);
+      const {identifier} = config(mappings, pathString)
 
-      const index = collection.findIndex(item => identifier in item);
-      if (index === NOT_FOUND) throw new Error(`Collection has no property: '${identifier}'`);
+      const index = collection.findIndex(item => identifier in item)
+      if (index === NOT_FOUND) throw new Error(`Collection has no property: '${identifier}'`)
 
-      const storeIds      = store.accessors.get(path).map(item => item[identifier]);
-      const collectionIds = collection.map(item => item[identifier]);
-      const idsToRemove   = storeIds.filter(id => collectionIds.indexOf(id) < 0);
+      const storeIds = store.accessors.get(path).map(item => item[identifier])
+      const collectionIds = collection.map(item => item[identifier])
+      const idsToRemove = storeIds.filter(id => collectionIds.indexOf(id) < 0)
 
-      if (!isEmpty(idsToRemove)) idsToRemove.forEach(id => this.update(pathString, id));
+      if (!isEmpty(idsToRemove)) idsToRemove.forEach(id => this.update(pathString, id))
 
-      const updated = collection.map(item => this.update(pathString, item[identifier], item, options));
-      return Promise.all(updated);
+      const updated = collection.map(item => this.update(pathString, item[identifier], item, options))
+      return Promise.all(updated)
     },
 
-    updateWhere(pathString, query, attrs = null, options = {replace: true}) {
-      const path = pathString.split('.');
-      const {identifier} = config(mappings, pathString);
-      let updated        = null;
+    updateWhere (pathString, query, attrs = null, options = {replace: true}) {
+      const path = pathString.split('.')
+      const {identifier} = config(mappings, pathString)
+      let updated = null
 
-      const item = queryStore('find', store.accessors.get(path), query);
-      if (!item) throw new Error(`No object found at path: '${pathString}' with query: '${JSON.stringify(query)}`);
+      const item = queryStore('find', store.accessors.get(path), query)
+      if (!item) throw new Error(`No object found at path: '${pathString}' with query: '${JSON.stringify(query)}`)
 
       if (isArray(store.accessors.get(path))) {
-        updated = updateCollection(store, pathString, item[identifier], identifier, attrs, options.replace);
+        updated = updateCollection(store, pathString, item[identifier], identifier, attrs, options.replace)
       } else {
-        updated = updateObject(store, pathString, attrs, options.replace);
+        updated = updateObject(store, pathString, attrs, options.replace)
       }
-      return Promise.resolve(updated);
+      return Promise.resolve(updated)
     }
   }
 }
 
-function updateObject(store, pathString, attrs, replace) {
-  const path = pathString.split('.');
+function updateObject (store, pathString, attrs, replace) {
+  const path = pathString.split('.')
 
   if (attrs === null) {
-    const deleted = store.accessors.get(path);
-    store.mutators.set(path, attrs);
-    return deleted;
+    const deleted = store.accessors.get(path)
+    store.mutators.set(path, attrs)
+    return deleted
   } else if (replace || attrs === null) {
-    store.mutators.set(path, attrs);
-    return attrs;
+    store.mutators.set(path, attrs)
+    return attrs
   } else {
-    store.mutators.merge(path, attrs);
-    return store.accessors.get(path);
+    store.mutators.merge(path, attrs)
+    return store.accessors.get(path)
   }
 }
 
-function updateCollection(store, pathString, id, identifier, attrs, replace) {
-  const path = pathString.split('.');
-  const index = store.accessors.get(path).findIndex(item => item[identifier] === id);
+function updateCollection (store, pathString, id, identifier, attrs, replace) {
+  const path = pathString.split('.')
+  const index = store.accessors.get(path).findIndex(item => item[identifier] === id)
 
   if (index === NOT_FOUND && attrs === null) {
-    throw new Error(`No object found at path: '${pathString}' with '${identifier}': ${id}.`);
+    throw new Error(`No object found at path: '${pathString}' with '${identifier}': ${id}.`)
   }
 
   if (index === NOT_FOUND) {
-    store.mutators.push(path, attrs);
-    return attrs;
+    store.mutators.push(path, attrs)
+    return attrs
   } else if (attrs === null) {
-    const deleted = store.accessors.get(path)[index];
-    store.mutators.splice(path, index, 1);
-    return deleted;
+    const deleted = store.accessors.get(path)[index]
+    store.mutators.splice(path, index, 1)
+    return deleted
   } else if (replace) {
-    store.mutators.splice(path, index, 1, attrs);
-    return attrs;
+    store.mutators.splice(path, index, 1, attrs)
+    return attrs
   } else {
-    const previous = assign({}, store.accessors.get(path)[index]);
-    const updated  = assign({}, previous, attrs);
-    store.mutators.splice(path, index, 1, updated);
-    return updated;
+    const previous = assign({}, store.accessors.get(path)[index])
+    const updated = assign({}, previous, attrs)
+    store.mutators.splice(path, index, 1, updated)
+    return updated
   }
 }
 
-function queryStore(method, cachedData, query) {
-  if (isEmpty(cachedData)) return null;
-  if (isEmpty(query)) return isEmpty(cachedData) ? null : cachedData;
-  let result;
+function queryStore (method, cachedData, query) {
+  if (isEmpty(cachedData)) return null
+  if (isEmpty(query)) return isEmpty(cachedData) ? null : cachedData
+  let result
 
   if (isArray(cachedData)) {
     result = cachedData[method].call(cachedData, (item) => {
       return Object.keys(query).reduce((bool, queryKey) => {
-        return bool && (item[queryKey] === query[queryKey]);
-      }, true);
-    });
+        return bool && (item[queryKey] === query[queryKey])
+      }, true)
+    })
   } else {
     const check = Object.keys(query).reduce((bool, queryKey) => {
-      return bool && (cachedData[queryKey] === query[queryKey]);
-    }, true);
-    result = check ? cachedData : null;
+      return bool && (cachedData[queryKey] === query[queryKey])
+    }, true)
+    result = check ? cachedData : null
   }
 
-  return isEmpty(result) ? null : result;
+  return isEmpty(result) ? null : result
 }
